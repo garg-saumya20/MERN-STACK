2f81dcebb97e3da56d675193479dd9d244e4ae83
Saumya Garg <garg.saumya20@gmail.com>
2021-06-21 13:39:01 +0530

    fifth

--- server/router/auth.js
+++ server/router/auth.js
@@ -2,19 +2,35 @@ const express = require('express');
 const router=express.Router();
 
 require('../db/conn');
-const User= require('../model/userSchema');
+const USER= require('../model/userSchema');
 
 router.get('/',(req,res)=>{
     res.send("hello world from get request on HOME page from router");
 });
 
 router.post('/register',(req,res)=>{
-  
-
+    const { name, email, phone, work, password, cpassword } = req.body;
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "Please filled all the fields" });
+    }
+    USER.findOne({ email: email })
+    .then((userExist) => {
+        if (userExist) {
+            return res.status(422).json({ error: "Email already exist" });
+        }
+        const user = new USER({ name, email, phone, work, password, cpassword });
+        user.save().then(() => {
+            res.status(201).json({ message: "User registered successfully" });
+        }).catch((err) => {
+            res.status(500).json({ error: "Failed to register" });
+        })
+    }).catch(err => {
+        console.log(err);
+    })
+   
     
-    console.log(req.body);
-    res.json({message:req.body})
-})
+    
+});
 
 
 module.exports = router;
