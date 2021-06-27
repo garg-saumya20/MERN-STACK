6ca2cc622cde20c09d37ae4e0d29688ef27c3d04
Saumya Garg <garg.saumya20@gmail.com>
2021-06-27 16:18:50 +0530

    Final

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,44 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import {useHistory} from "react-router-dom";
 function Login() {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const history=useHistory();
+    const loginUser=async (e)=>
+    {
+e.preventDefault();
+const res=await fetch("/signin",{
+    method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email,password
+            })
+        });
+        const data=res.json();
+        if(res.status===400 || !data)
+        {
+            window.alert("Invalid credentials...");
+        }
+        else
+        {
+           window.alert("Login Successfuly...");
+           history.push("/");
+        }
+
+    }
     return (
         <div>
             Login
             <br/>
-            <input type="text" name="email" id="email" autocomplete="off" placeholder="Your email" />
+            <form method="POST">
+            <input type="text" name="email" id="email" autocomplete="off" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Your email" />
                 <br/>
-                <input type="password" name="password" id="password" autocomplete="off" placeholder="Password" />
+                <input type="password" name="password" id="password" autocomplete="off" value={password} onChange={(e)=>setPassword(e.target.value)}  placeholder="Password" />
                 <br/>
-                <button>Signin</button>
-                
+                <button onClick={loginUser}>Signin</button>
+                </form>
         </div>
     );
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,41 +1,66 @@
-import React from 'react';
-import {useState} from "react";
+import React, { useState} from 'react';
+import { useHistory } from 'react-router-dom';
+
 function Signup() {
-    const [user,setUser]=useState({
-        name:"",
-        email:"",
-        phone:"",
-        work:"",
-        password:"",
-        cpassword:""
+    const history=useHistory();
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        work: "",
+        password: "",
+        cpassword: "",
     });
-    let name,value;
-    function handleInputs(e)
-    {
-        console.log(e);
-        name=e.target.name;
-        value=e.target.value;
-        setUser({...user,[name]:value});
+    let name, value;
+    const handleInputs = (event) => {
+        console.log("garg")
+        console.log(event.target.value);
+        name = event.target.name;
+        value = event.target.value;
+        setUser({ ...user, [name]: value });
+    }
+    const PostData = async (e) => {
+        e.preventDefault();
+        const { name, email, phone, work, password, cpassword } = user;
+        const res = await fetch("/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name, email, phone, work, password, cpassword
+            })
+        });
+        const data=await res.json();
+        if(data.status===422 || !data)
+        {
+             window.alert("Invalid Registration");
+             console.log("Invalid Registration")
+        }
+        else{
+            window.alert("Registration Successfull...");
+            console.log("Registration Successfull...");
+            history.push("/login")
+        }
     }
     return (
         <>
-        SignUp
-            <div className="Signup">
-                <input type="text" name="name" id="name" autocomplete="off" value={user.name} onchange={handleInputs} placeholder="Your name" />
-                <br/>
-                <input type="text" name="email" id="email" autocomplete="off" value={user.email} onchange={handleInputs} placeholder="Email" />
-                <br/>
-                <input type="text" name="phone" id="phone" autocomplete="off" value={user.phone} onchange={handleInputs} placeholder="Phone" />
-                <br/>
-                <input type="text" name="work" id="work" autocomplete="off" value={user.work} onchange={handleInputs} placeholder="Profession" />
-                <br/>
-                <input type="password" name="password" id="password" autocomplete="off" value={user.password} onchange={handleInputs} placeholder="Password" />
-                <br/>
-                <input type="password" name="cpassword" id="cpassword" autocomplete="off" value={user.cpassword} onchange={handleInputs} placeholder="Confirm Password" />
-                <br/>
-                <button>Register</button>
-                
-            </div>
+            SignUp
+            <form method="POST">
+                <input type="text" name="name" id="name" autocomplete="off" value={user.name} onChange={handleInputs} placeholder="Your name" />
+                <br />
+                <input type="email" name="email" id="email" autocomplete="off" value={user.email} onChange={handleInputs} placeholder="Email" />
+                <br />
+                <input type="text" name="phone" id="phone" autocomplete="off" value={user.phone} onChange={handleInputs} placeholder="Phone" />
+                <br />
+                <input type="text" name="work" id="work" autocomplete="off" value={user.work} onChange={handleInputs} placeholder="Profession" />
+                <br />
+                <input type="password" name="password" id="password" autocomplete="off" value={user.password} onChange={handleInputs} placeholder="Password" />
+                <br />
+                <input type="password" name="cpassword" id="cpassword" autocomplete="off" value={user.cpassword} onChange={handleInputs} placeholder="Confirm Password" />
+                <br />
+                <button onClick={PostData}>Register</button>
+            </form>
         </>
     );
 }
